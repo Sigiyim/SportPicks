33b21d9952063309f05b1fccac842c60b9795d2b
Sigiyim <GavinMcDevitt@gmail.com>
2014-10-07 13:32:05 -0400

    Some view functionality added.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var http = require('http');
 var express = require('express');
+var path = require("path");
+
 var app = express();
 
-var port = process.env.port || 3000;
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.get('/', function(req, res) {
-    res.send("Hello, World!");
+app.get('/user/new', function(req, res) {
+    res.render('user/new', {});
 });
 
-var server = app.listen(port, function() {
-    console.log('App listening on port %d', port);
+var server = app.listen(app.get('port'), function() {
+    console.log('App listening on port %d', app.get('port'));
 });
