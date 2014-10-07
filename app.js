e129cede4e21b06881aa2bf3439d90148c8dcd79
Sigiyim <GavinMcDevitt@gmail.com>
2014-10-07 14:17:52 -0400

    Can submit a new user, but not to the database yet.

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+var Db = require('mongodb').Db;
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require("path");
 
 var app = express();
+var connectionString = process.env.CUSTOMCONNSTR_SPORTPICKS || 'mongodb://127.0.0.1/SportPicks';
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.get('/user/new', function(req, res) {
-    res.render('user/new', {});
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
-var server = app.listen(app.get('port'), function() {
-    console.log('App listening on port %d', app.get('port'));
+Db.connect(connectionString, function(err, db) {
+    app.all('*', function(req, res, next) {
+        req.db = db;
+
+        next();
+    });
+
+    app.get('/user/new', function(req, res) {
+        res.render('user/new', {});
+    });
+    app.post('/user/new', function(req, res) {
+        var body = req.body;
+
+        if ( body.password == body.password2 ) {
+            res.send("You did it");
+        } else {
+            res.render('user/new', {});
+        }
+    });
+
+    var server = app.listen(app.get('port'), function() {
+        console.log('App listening on port %d', app.get('port'));
+    });
 });
