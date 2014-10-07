1111ccfde62c81d5f75ad3dd9cb968e6762cb723
Sigiyim <GavinMcDevitt@gmail.com>
2014-10-07 10:02:29 -0400

    Simpler example

--- site/app.js
+++ site/app.js
@@ -1,15 +1,12 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.port || 3000);
+var port = process.env.port || 3000;
 
-app.get('/', function(res, res) {
-    res.send()
-})
-app.get('/hello', function(req, res) {
-    res.send('Hello, World!');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type' : 'text/plain' });
+    res.end('Hello, World\n');
 });
 
-var server = app.listen(app.get('port'), function() {
-    console.log('Listening on port %d', server.address().port);
-});
+server.listen(port);
